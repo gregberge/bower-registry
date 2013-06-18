d9f3b978822c129c08e578d68e601dfe430245d4
Greg Bergé <neoziro@gmail.com>
2013-06-18 18:08:25 +0200

    add search endpoint

--- test/registry.js
+++ test/registry.js
@@ -29,9 +29,9 @@ describe('Registry server', function () {
 
     it('should show all avalaible packages', function (done) {
       request(this.registry.server)
-      .get('/packages')
-      .expect('Content-type', /json/)
-      .expect(200, this.adapter.packages, done);
+        .get('/packages')
+        .expect('Content-type', /json/)
+        .expect(200, this.adapter.packages, done);
     });
   });
 
@@ -39,18 +39,18 @@ describe('Registry server', function () {
     describe('if request is correct', function () {
       it('should add the package and return 200', function (done) {
         request(this.registry.server)
-        .post('/packages')
-        .send({name: 'jquery', url: 'git://github.com/jquery/jquery.git'})
-        .expect(200, done);
+          .post('/packages')
+          .send({name: 'jquery', url: 'git://github.com/jquery/jquery.git'})
+          .expect(200, done);
       });
     });
 
     describe('if request is bad', function () {
       it('shouldn\'t add the package and return 400', function (done) {
         request(this.registry.server)
-        .post('/packages')
-        .send({name: 'jquery'})
-        .expect(400, done);
+          .post('/packages')
+          .send({name: 'jquery'})
+          .expect(400, done);
       });
     });
 
@@ -67,23 +67,22 @@ describe('Registry server', function () {
       describe('if the name exists', function () {
         it('should return 406', function (done) {
           request(this.registry.server)
-          .post('/packages')
-          .send({name: 'jquery', url: 'git://github.com/jquery2/jquery2.git'})
-          .expect(406, done);
+            .post('/packages')
+            .send({name: 'jquery', url: 'git://github.com/jquery2/jquery2.git'})
+            .expect(406, done);
         });
       });
 
       describe('if the url exists', function () {
         it('should return 406', function (done) {
           request(this.registry.server)
-          .post('/packages')
-          .send({name: 'jquery2', url: 'git://github.com/jquery/jquery.git'})
-          .expect(406, done);
+            .post('/packages')
+            .send({name: 'jquery2', url: 'git://github.com/jquery/jquery.git'})
+            .expect(406, done);
         });
       });
     });
   });
-
 
   describe('GET /packages/:name', function () {
     describe('if the package exists', function () {
@@ -102,6 +101,30 @@ describe('Registry server', function () {
           .expect(200, this.adapter.packages[0], done);
       });
     });
+
+    describe('if the package doesn\'t exist', function () {
+      it('should return 404', function (done) {
+        request(this.registry.server)
+          .get('/packages/jquery')
+          .expect(404, done);
+      });
+    });
   });
 
+  describe('GET /packages/search/:name', function () {
+    beforeEach(function () {
+      this.adapter.packages = [
+        {
+          name: 'jquery',
+          url: 'git://github.com/jquery/jquery.git'
+        }
+      ];
+    });
+
+    it('should return matching packages', function (done) {
+      request(this.registry.server)
+        .get('/packages/search/jq')
+        .expect(200, this.adapter.packages, done);
+    });
+  });
 });
