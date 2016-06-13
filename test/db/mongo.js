ac713ef0a8772aff46f97f24465e0b5ddb254cca
bmorlac <bmorlac@users.noreply.github.com>
2016-06-13 20:39:38 +0200

    Tests revised

--- test/db/mongo.js
+++ test/db/mongo.js
@@ -3,34 +3,38 @@
 var Q = require('q'),
     MongoDb = require('../../index').MongoDb,
     mockgo = require('mockgo'),
-    should = require('chai').should();
+    expect = require('chai').expect;
+
+require('chai').should();
 
 describe('MongoDb', function () {
+  var connection, db;
   this.timeout(0);
 
-  beforeEach(function (done) {
-    var defer = Q.defer();
-    mockgo.getConnection(function(err, db) {
-      if (err) {
-        return defer.reject();
-      }
-
-      defer.resolve(db);
+  function connect(done) {
+    mockgo.getConnection(function(error, _connection) {
+      expect(error).to.be.null;
+      connection = _connection;
+      db = new MongoDb({
+        url: 'mongodb://localhost:8000/testDatabase',
+        client: Q.when(connection)
+      });
       done();
     });
-    this.db = new MongoDb({
-      url: 'mongodb://localhost:8000/testDatabase',
-      client: defer.promise
-    });
-  });
+  }
 
-  afterEach(function (done) {
+  function disconnect(done) {
     mockgo.shutDown(done);
-  });
+  }
 
   describe('#find', function () {
-    beforeEach(function (done) {
-      this.packages = [
+    var packages;
+
+    before(connect);
+    after(disconnect);
+
+    beforeEach(function () {
+      packages = [
         {
           name: 'jquery',
           url: 'git://github.com/jquery/jquery.git'
@@ -41,53 +45,48 @@ describe('MongoDb', function () {
         }
       ];
 
-      var client = this.db.client,
-          packages = this.packages;
-      client.then(function (db) {
-        db.collection('packages').updateOne(packages[0], { $set: packages[0] }, {upsert: true});
-        db.collection('packages').updateOne(packages[1], { $set: packages[1] }, {upsert: true});
-        done();
-      });
+      connection.collection('packages').updateOne(packages[0], { $set: packages[0] }, {upsert: true});
+      connection.collection('packages').updateOne(packages[1], { $set: packages[1] }, {upsert: true});
     });
 
     describe('without arguments', function () {
       it('should return all packages', function (done) {
-        this.db.find().then(function (packages) {
+        db.find().then(function (packages) {
           packages.forEach(function (package, i) {
-            package.should.have.property('name', this.packages[i].name);
-            package.should.have.property('url', this.packages[i].url);
-          }.bind(this));
+            package.should.have.property('name', packages[i].name);
+            package.should.have.property('url', packages[i].url);
+          });
           done();
-        }.bind(this));
+        });
       });
     });
 
     describe('with a hash matching one value', function () {
       it('should return packages matching the query', function (done) {
-        this.db.find({
+        db.find({
           name: 'jquery'
         }).then(function (packages) {
           packages[0].should.have.property('name', 'jquery');
           packages[0].should.have.property('url', 'git://github.com/jquery/jquery.git');
           done();
-        }.bind(this));
+        });
       });
     });
 
     describe('with a hash matching 0 value', function () {
       it('should not return packages', function (done) {
-        this.db.find({
+        db.find({
           name: 'jquery2'
         }).then(function (packages) {
           packages.length.should.equal(0);
           done();
-        }.bind(this));
+        });
       });
     });
 
     describe('with a hash with "$match" operator', function () {
       it('should return packages matching the query', function (done) {
-        this.db.find({
+        db.find({
           $match: {
             name: 'jq'
           }
@@ -101,18 +100,17 @@ describe('MongoDb', function () {
   });
 
   describe('#add', function () {
+    before(connect);
+    after(disconnect);
+
     describe('if package exist', function () {
-      beforeEach(function (done) {
-        var client = this.db.client;
-        client.then(function (db) {
-          db.collection('packages').update({name: 'jquery'}
-            , { $set: {url: 'git://jquery.git', name: 'jquery'} }, {upsert: true});
-          done();
-        });
+      before(function () {
+        connection.collection('packages').update({name: 'jquery'}
+          , { $set: {url: 'git://jquery.git', name: 'jquery'} }, {upsert: true});
       });
 
       it('should return an error', function (done) {
-        this.db.add({
+        db.add({
           name: 'jquery',
           url: 'git://github.com/jquery2/jquery2.git'
         }).then(function () {}, function () {
@@ -123,38 +121,34 @@ describe('MongoDb', function () {
 
 
     it('should add the package', function (done) {
-      this.db.add({
+      db.add({
         name: 'jquery',
         url: 'git://github.com/jquery/jquery.git'
       }).then(function () {
-        this.db.client.then(function (db) {
-          db.collection('packages').findOne({name: 'jquery'}, function(err, res) {
-            should.exist(res);
-            done();
-          });
+        connection.collection('packages').findOne({name: 'jquery'}, function(err, res) {
+          expect(res).not.to.be.null;
+          done();
         });
-      }.bind(this));
+      });
     });
   });
 
   describe('#hit', function () {
+    before(connect);
+    after(disconnect);
+
     beforeEach(function () {
-      this.db.client.then(function (db) {
-        db.collection('packages').update({name: 'jquery'}
-          , { $set: {url: 'git://jquery.git', name: 'jquery'} }, {upsert: true});
-        done();
-      });
+      connection.collection('packages').update({name: 'jquery'}
+        , { $set: {url: 'git://jquery.git', name: 'jquery'} }, {upsert: true});
     });
 
     it('should increment package hit value', function (done) {
-      this.db.hit('jquery').then(function () {
-        this.db.client.then(function (db) {
-          db.collection('packages').findOne({name: 'jquery'}, function(err, pkg) {
-            pkg.hits.should.equal(1);
-            done();
-          });
+      db.hit('jquery').then(function () {
+        connection.collection('packages').findOne({name: 'jquery'}, function(err, pkg) {
+          pkg.hits.should.equal(1);
+          done();
         });
-      }.bind(this));
+      });
     });
   });
 
